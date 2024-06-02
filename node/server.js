fbb463e358ad1d22336f120c00874b56ffeb7830
sabiha <101255170+sabihamohammed@users.noreply.github.com>
2024-06-01 23:48:28 -0400

    Add files via upload

---
+++ node/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { connectDB } = require('./Config/db');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Import route handlers
+const authRoutes = require('./routes/auth');
+
+
+// Create Express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use('/auth', authRoutes);
+
+
+// Serve static files
+app.use(express.static('public'));
+
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
